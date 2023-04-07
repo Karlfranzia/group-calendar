969d990327b34edfd57bf2c670df6d415eadee07
jessie-gibbs <jessiecorbingibbs@gmail.com>
2023-04-06 19:01:59 -0700

    updated boilerplate code

---
+++ controllers/api/eventRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { Event } = require('../../models/');
+const dayjs = require('dayjs')
+
+router.post("/create-event", async(req, res) => {
+    const event = Event(req.body);
+    await event.save();
+    res.sendStatus(201)
+});
+
+router.get("get-events", async (req, res) => {
+    try{
+        const events = await Event.findAll({
+        where: {
+            start: {
+                [Op.gte]: dayjs(req.query.start).toDate()
+            },
+            end: {
+                [Op.lte]: dayjs(req.query.end).toDate()
+            }
+        }
+        });
+        res.json(events);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching events');
+      }   
+    
+  });
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Event = require('./event')
+module.exports = { User };
+module.exports = { Event };
