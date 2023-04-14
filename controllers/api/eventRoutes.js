ccb5e944b9d3903e98d567f4dc2178df07e61d6b
Bryan-Codes <bry5884@gmail.com>
2023-04-13 21:41:02 -0700

    Fixed typos

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -3,32 +3,30 @@ const { Event } = require('../../models/');
 const dayjs = require('dayjs')
 const { Op } = require('sequelize');
 
-router.post("/create-event", async(req, res) => {
+router.post("/create-event", async (req, res) => {
     const event = new Event(req.body);
     await event.save();
     res.sendStatus(201)
 });
 
 router.get("/get-events", async (req, res) => {
-    try{
+    try {
         const events = await Event.findAll({
-        where: {
-            start: {
-                [Op.gte]: dayjs(req.query.start).toDate()
-            },
-            end: {
-                [Op.lte]: dayjs(req.query.end).toDate()
+            where: {
+                start: {
+                    [Op.gte]: dayjs(req.query.start).toDate()
+                },
+                end: {
+                    [Op.lte]: dayjs(req.query.end).toDate()
+                }
             }
-        }
         });
-        
-        const events = await Event.findAll();
         res.json(events);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error fetching events');
-      }   
-    
-  });
+    }
+
+});
 
 module.exports = router;
